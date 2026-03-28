24c10758213f9ac5330017ad5e66c91a4e3dd8fe
OleksiukRoman <romanoleksuk10@gmail.com>
2026-03-28 22:02:31 +0200

    define FileEntity and register it in FilesModule

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -1,1 +1,26 @@
-export class File {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+import { User } from '../../auth/entities/user.entity';
+
+@Entity('files')
+export class FileEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  filename: string; 
+
+  @Column()
+  path: string; 
+
+  @Column()
+  mimetype: string; 
+
+  @Column()
+  size: number; 
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => User)
+  user: User;
+}
--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
-
+import { FileEntity } from './entities/file.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([FileEntity])],
   controllers: [FilesController],
   providers: [FilesService],
 })
