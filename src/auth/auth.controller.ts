d744eb8774d9dbf4fa6b2f7d60953a2ac4f598e0
OleksiukRoman <romanoleksuk10@gmail.com>
2026-03-28 12:40:14 +0200

    complete AuthController with register endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Post('register') 
+  @ApiOperation({ summary: 'Реєстрація нового користувача' }) 
+  create(@Body() createAuthDto: CreateAuthDto) {
+    
+    return this.authService.create(createAuthDto);
+  }
 }
