599e803e59b15a3c07e11044705f3ecf9b869d04
OleksiukRoman <romanoleksuk10@gmail.com>
2026-03-28 12:57:12 +0200

    add password hashing with bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,8 +13,21 @@ constructor(
 ) {}
 
 
-async create(dto: CreateAuthDto) {
-    const user = this.userRepository.create(dto); 
-    return await this.userRepository.save(user); 
+async create(createAuthDto: CreateAuthDto) {
+    const { email, password } = createAuthDto;
+
+    
+    const salt = await bcrypt.genSalt();
+    
+    
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    
+    const user = this.userRepository.create({
+    email,
+    password: hashedPassword,
+    });
+
+    return await this.userRepository.save(user);
 }
 }
