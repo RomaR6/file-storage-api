f24f45e0e58dc7418a7467aeb68ef6750f57ae80
OleksiukRoman <romanoleksuk10@gmail.com>
2026-03-28 12:45:39 +0200

    enable Swagger documentation at /api route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('File Storage API') 
+    .setDescription('Документація API для файлового сховища')
+    .setVersion('1.0')
+    .addTag('auth') 
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: http://localhost:3000/api`);
 }
 bootstrap();
