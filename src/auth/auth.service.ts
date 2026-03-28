1f621ea0bc4f78d4558f27bd8d359d1838514ebf
OleksiukRoman <romanoleksuk10@gmail.com>
2026-03-28 11:51:51 +0200

    add create user logic to AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateAuthDto } from './dto/create-auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>, 
+) {}
+
+
+async create(dto: CreateAuthDto) {
+    const user = this.userRepository.create(dto); 
+    return await this.userRepository.save(user); 
+}
+}
