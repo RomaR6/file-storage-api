e3e447ee063abcd2324bb2d94ea367a331862bed
OleksiukRoman <romanoleksuk10@gmail.com>
2026-03-29 12:48:42 +0300

    save file information to database after upload

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -4,19 +4,20 @@ import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
+import { FilesService } from './files.service';
 
 @ApiTags('files')
 @Controller('files')
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard) 
 export class FilesController {
-  
+  constructor(private readonly filesService: FilesService) {}
   @Post('upload')
   @UseInterceptors(FileInterceptor('file', {
     storage: diskStorage({
       destination: './uploads', 
       filename: (req, file, cb) => {
-        // Генеруємо ім'я: Таймстамп-Рандом.розширення
+        
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
         cb(null, `${uniqueSuffix}${extname(file.originalname)}`);
       },
@@ -34,13 +35,7 @@ export class FilesController {
       },
     },
   })
-  uploadFile(@UploadedFile() file: Express.Multer.File, @Request() req) {
-    
-    return {
-      userId: req.user.userId,
-      filename: file.filename,
-      size: file.size,
-      mimetype: file.mimetype,
-    };
+  async uploadFile(@UploadedFile() file: Express.Multer.File, @Request() req) {
+    return this.filesService.create(file, req.user.userId);
   }
 }
