13b39d585994a1aec9b677b6c55db32640db8fc1
OleksiukRoman <romanoleksuk10@gmail.com>
2026-03-29 12:26:20 +0300

    implement file upload to disk using Multer

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { FilesService } from './files.service';
-import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
+import { Controller, Post, UseInterceptors, UploadedFile, UseGuards, Request } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
+@ApiTags('files')
 @Controller('files')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard) 
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
-
-  @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.filesService.create(createFileDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.filesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.filesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-    return this.filesService.update(+id, updateFileDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.filesService.remove(+id);
+  
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads', 
+      filename: (req, file, cb) => {
+        // Генеруємо ім'я: Таймстамп-Рандом.розширення
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, `${uniqueSuffix}${extname(file.originalname)}`);
+      },
+    }),
+  }))
+  @ApiConsumes('multipart/form-data') 
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Request() req) {
+    
+    return {
+      userId: req.user.userId,
+      filename: file.filename,
+      size: file.size,
+      mimetype: file.mimetype,
+    };
   }
 }
